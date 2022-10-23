e857ee4f2b6a70ecef0166e946d7ef7ab48f1434
edsonibarra <edsonibarra97@gmail.com>
2022-10-23 17:11:44 -0500

    Better Tests

---
+++ test_validSubsequence.py
@@ -0,0 +1,19 @@
+import pytest
+from validSubsequence import validSubsequence
+
+
+@pytest.mark.parametrize(
+    ('array', 'subsequence', 'expected'),
+    (
+        ([1,2,3,4,5,6,7,8,9], [7,8,9], True),
+        ([1,2,3,4,5,6,7,8,9], [7,8], True),
+        ([1,2,3,4,5,6,7,8,9], [7], True),
+        ([1,2,3,4,5,6,7,8,9], [], False),
+        ([1,2,3,4,5,6,7,8,9], [10,11,12], False),
+        ([1], [1], True),
+        ([], [7,8,9], False),
+    )
+)
+def test_valid_subsequence(array, subsequence, expected):
+
+    assert validSubsequence(array, subsequence) == expected
--- validSubsequence.py
+++ validSubsequence.py
@@ -1,14 +1,14 @@
 def validSubsequence(array, sequence):
     pointerArray = 0
     pointerSquence = 0
-    completedSuccess = len(sequence) - 1
+    completedSuccess = len(sequence)
     while pointerSquence < len(sequence) and pointerArray < len(array):
         if sequence[pointerSquence] == array[pointerArray]:
             pointerArray += 1
             pointerSquence += 1
         else:
             pointerArray += 1
-    
-    if pointerSquence == completedSuccess:
+    print(pointerSquence, completedSuccess)
+    if sequence and pointerSquence == completedSuccess:
         return True
     return False
