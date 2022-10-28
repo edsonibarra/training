4f500ad31111544f797b397b1d688b377b87c625
edsonibarra <edsonibarra97@gmail.com>
2022-10-28 18:24:00 -0500

    palindrome check solved

---
+++ palindrome_check.py
@@ -0,0 +1,8 @@
+def isPalindrome(string: str) -> bool:
+
+    cleaned_string = ""
+    for c in string:
+        if c.isalnum():
+            cleaned_string += c
+
+    return cleaned_string == cleaned_string[::-1]
