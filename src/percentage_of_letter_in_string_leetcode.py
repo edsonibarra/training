65712016808b3096cb8f2a45a4561f973ddc9106
edsonibarra <edsonibarra97@gmail.com>
2022-10-30 20:23:13 -0600

    solved using brute force

---
+++ src/percentage_of_letter_in_string_leetcode.py
@@ -0,0 +1,27 @@
+# Brute Force just to solve it in time I'll add an optimized
+# solution
+
+# def percentage_of_letter_in_string(s, letter):
+    
+#     total = len(s)
+    
+#     count_letter_in_s = 0
+#     for c in s:
+#         if c == letter:
+#             count_letter_in_s += 1
+    
+#     p = int(str(count_letter_in_s/total * 100).split(".")[0])
+#     print(p)
+    
+    
+# percentage_of_letter_in_string('foobar','o')
+
+
+def calculate_percentage_optimized(s, letter):
+    """An optimized version for this leetcode challenge
+
+    Args:
+        s (str): string
+        letter (str): letter
+    """
+    pass
