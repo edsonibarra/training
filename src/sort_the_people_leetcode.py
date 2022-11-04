266a91e31a8058a275e20605feb44348eca724bf
edsonibarra <edsonibarra97@gmail.com>
2022-11-04 09:52:29 -0600

    solved

--- src/sort_the_people_leetcode.py
+++ src/sort_the_people_leetcode.py
@@ -1,2 +1,15 @@
 def sort_the_people(names, heights):
-    pass
+
+    heights_names = zip(heights, names)
+    heights_first = []
+
+    for height_name in heights_names:
+        heights_first.append(height_name)
+        
+    answer_sorted = sorted(heights_first)
+    
+    answer = []
+    for name in answer_sorted[::-1]:
+        answer.append(name[1])
+    return answer  
+    
