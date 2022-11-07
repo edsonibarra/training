211024986f6c95bf9444c9e7094cffe822313e0b
edsonibarra <edsonibarra97@gmail.com>
2022-11-07 00:27:04 -0600

    use hash tables

--- src/group_anagrams.py
+++ src/group_anagrams.py
@@ -1,25 +1,18 @@
-def group_anagrams():
+def group_anagrams(strs):
     """Given an array of strings strs, group the anagrams together.
     You can return the answer in any order.
     An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase, typically using all the original letters exactly once.
     Input: strs = ["eat","tea","tan","ate","nat","bat"]
     Output: [["bat"],["nat","tan"],["ate","eat","tea"]]
     """
-    
-    pass
+    anagrams = {}
+    for word in strs:
+        sorted_word = sorted(word)
+        if sorted_word in anagrams:
+            anagrams[sorted_word].append(word)
+        else:
+            anagrams[sorted_word] = [word]
+    return list(anagrams.values)
 
 
-def get_chars(word):
-    """Return a dict with the letters of the word and the count
-    of the letters as value
-
-    Args:
-        word : word to get the chars from
-
-    Returns:
-        dict: letters
-    """
-    d = {}
-    for c in word:
-        d[c] = d.get(c,0) + 1
-    return d
+print(group_anagrams(["eat","tea","tan","ate","nat","bat"]))
