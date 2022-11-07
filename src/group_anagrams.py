9975d5257362877caeb3b11888cc872c3272db1f
edsonibarra <edsonibarra97@gmail.com>
2022-11-06 21:08:09 -0600

    get chars

--- src/group_anagrams.py
+++ src/group_anagrams.py
@@ -7,3 +7,19 @@ def group_anagrams():
     """
     
     pass
+
+
+def get_chars(word):
+    """Return a dict with the letters of the word and the count
+    of the letters as value
+
+    Args:
+        word : word to get the chars from
+
+    Returns:
+        dict: letters
+    """
+    d = {}
+    for c in word:
+        d[c] = d.get(c,0) + 1
+    return d
