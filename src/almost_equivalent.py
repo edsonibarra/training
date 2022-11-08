62f1640c9988be1f84270799cf7a5fa845b58129
edsonibarra <edsonibarra97@gmail.com>
2022-11-07 23:53:51 -0600

    optimized solution

--- src/almost_equivalent.py
+++ src/almost_equivalent.py
@@ -1,28 +1,44 @@
-def check_almost_equivalent(word1, word2):
+# def check_almost_equivalent(word1, word2):
 
+#     d1 = {}
+#     for c in word1:
+#         d1[c] = d1.get(c,0)+1
+    
+#     d2 = {}
+    
+#     for c in word2:
+#         d2[c] = d2.get(c,0)+1
+    
+#     for c in d1:
+#         if c in d2 and abs(d1[c] - d2[c]) > 3:
+#             return False
+#         elif  c not in d2 and d1[c] > 3:
+#             return False
+        
+#     for c in d2:
+#         if c in d1 and abs(d1[c] - d2[c]) > 3:
+#             return False
+#         elif  c not in d1 and d2[c] > 3:
+#             return False
+        
+#     return True
+
+
+def check_almost_equivalent(word1, word2):
+    """Optimized version of the above
+    """
+    
     d1 = {}
     for c in word1:
         d1[c] = d1.get(c,0)+1
-    
-    d2 = {}
-    
     for c in word2:
-        d2[c] = d2.get(c,0)+1
-    
-    for c in d1:
-        if c in d2 and abs(d1[c] - d2[c]) > 3:
+        d1[c] = d1.get(c,0)-1
+    values = d1.values()
+    for n in values:
+        if abs(n) > 3:
             return False
-        elif  c not in d2 and d1[c] > 3:
-            return False
-        
-    for c in d2:
-        if c in d1 and abs(d1[c] - d2[c]) > 3:
-            return False
-        elif  c not in d1 and d2[c] > 3:
-            return False
-        
     return True
-
+        
 
 # print(check_almost_equivalent('aaaa','bccb'))
 # print(check_almost_equivalent('abcdeef','abaaacc'))
@@ -35,4 +51,3 @@ print(check_almost_equivalent('abcdeef','abaaacc'))
 
 
             
-        
