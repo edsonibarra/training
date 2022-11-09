1290148819cc9e75b304e51f119b5e6a77fcfd31
edsonibarra <edsonibarra97@gmail.com>
2022-11-08 18:27:28 -0600

    first approach

--- src/is_prefix_of_word.py
+++ src/is_prefix_of_word.py
@@ -1,2 +1,8 @@
 def is_prefix_of_word(sentence, search_word):
-    pass
+    if search_word in sentence:
+        words_list = sentence.split(" ")
+        for i, word in enumerate(words_list):
+            index = word.find(search_word)
+            if index == 0:
+                return i
+    return -1
