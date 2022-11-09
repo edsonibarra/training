f3dbceaeef6e25982b59237e258208cdc0913276
edsonibarra <edsonibarra97@gmail.com>
2022-11-08 18:31:17 -0600

    solved first approach brute force

--- src/is_prefix_of_word.py
+++ src/is_prefix_of_word.py
@@ -4,5 +4,10 @@ def is_prefix_of_word(sentence, search_word):
         for i, word in enumerate(words_list):
             index = word.find(search_word)
             if index == 0:
-                return i
+                return i + 1
     return -1
+
+
+print(is_prefix_of_word(sentence="i love eating burger", search_word="burg"))
+print(is_prefix_of_word(sentence="this problem is an easy problem", search_word="pro"))
+print(is_prefix_of_word(sentence="i am tired", search_word="you"))
