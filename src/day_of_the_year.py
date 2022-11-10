66c98d59529656819042a26a2e18e8ace1f73d2c
edsonibarra <edsonibarra97@gmail.com>
2022-11-09 21:50:17 -0600

    leap

--- src/day_of_the_year.py
+++ src/day_of_the_year.py
@@ -5,7 +5,20 @@ def day_of_day(date):
     day = int(date_list[2])
 
     leap = False
-    if year % 4 == 0 and year % 100 == 0 and year % 400 == 0:
+    # Python program to check if year is a leap year or not
+
+    year = 2000
+
+    # To get year (integer input) from the user
+    # year = int(input("Enter a year: "))
+
+    # divided by 100 means century year (ending with 00)
+    # century year divided by 400 is leap year
+    if (year % 400 == 0) and (year % 100 == 0):
+        leap = True
+    # not divided by 100 means not a century year
+    # year divided by 4 is a leap year
+    elif (year % 4 == 0) and (year % 100 != 0):
         leap = True
 
     months = [
