ef4296c1f420b3acd26d00539e22c3829483d802
edsonibarra <edsonibarra97@gmail.com>
2022-11-09 21:25:52 -0600

    leap

--- src/day_of_the_year.py
+++ src/day_of_the_year.py
@@ -1,6 +1,6 @@
 def day_of_day(date):
     leap = False
     date_list = date.split("-")
-    year = date_list[0]
-    month = date_list[1]
-    day = date_list[2]
+    year = int(date_list[0])
+    month = int(date_list[1])
+    day = int(date_list[2])
