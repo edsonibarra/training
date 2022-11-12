5f14abeaf4a1119a4583ddd98f817483e0f18c77
edsonibarra <edsonibarra97@gmail.com>
2022-11-12 01:53:30 -0600

    initial-commit

---
+++ src/challenges/missing_number.py
@@ -0,0 +1,15 @@
+from typing import List
+
+
+def missing_number(nums: List[int]) -> int:
+    """Given an array nums containing n distinct 
+    numbers in the range [0, n], return the only number 
+    in the range that is missing from the array.
+
+    Args:
+        nums (List[int]): numbers list
+
+    Returns:
+        int: missing number in nums
+    """
+    pass
