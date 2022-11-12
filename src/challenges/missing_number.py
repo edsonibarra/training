fad822a25c1c8852de2be61453ebcfca4dd296b3
edsonibarra <edsonibarra97@gmail.com>
2022-11-12 02:25:41 -0600

    challenge solved using two pointers approach

--- src/challenges/missing_number.py
+++ src/challenges/missing_number.py
@@ -12,4 +12,21 @@ def missing_number(nums: List[int]) -> int:
     Returns:
         int: missing number in nums
     """
-    pass
+
+    nums.sort()
+    l = [n for n in range(len(nums) + 1)]
+    a = 0
+    b = 0
+    while a < len(nums):
+        if nums[a] == l[b]:
+            print('cierto')
+            a += 1
+            b += 1
+        else:
+            return l[b]
+    return l[a]
+
+print(missing_number([0,3,1]))
+print(missing_number([3,0,1]))
+print(missing_number([9,6,4,2,3,5,7,0,1]))
+print(missing_number([0,1]))
