424ba41615b94b42bfe39a48d58122826e093c6f
edsonibarra <edsonibarra97@gmail.com>
2022-11-23 19:55:04 -0600

    solved

--- src/challenges/fizzbuzz.py
+++ src/challenges/fizzbuzz.py
@@ -1,2 +1,12 @@
 def fizzbuzz(n):
-    pass
+    ans = []
+    for n in range(1, n + 1):
+        if n % 3 == 0 and n % 5 == 0:
+            ans.append('FizzBuzz')
+        elif n % 3 == 0:
+            ans.append('Fizz')
+        elif n % 5 == 0:
+            ans.append('Buzz')
+        else:
+            ans.append(str(n))
+    return ans
