66d247767ea195a914695896ed75fd5d7edf542f
edson ibarra <edsonibarra97@gmail.com>
2022-12-22 21:11:45 -0600

    addAtTail complete

--- src/challenges/design_linked_list.py
+++ src/challenges/design_linked_list.py
@@ -1,21 +1,58 @@
+class Node:
+    def __init__(self, val):
+        self.val = val
+        self.next = None
+
+
 class MyLinkedList:
+
     def __init__(self):
         self.head = None
 
-    def get(self, index):
+    def get(self, index: int) -> int:
         count = 0
         if index == count:
-            
+            return self.head
+        cur = self.head
+        while cur and count != index:
+            count += 1
+            cur = cur.next
+        
+        if cur:
+            return cur.val
+        
+    def addAtHead(self, val: int) -> None:
+        new_node = Node(val)
+        if self.head is None:
+            self.head = new_node
+            return
+        cur = self.head
+        new_node.next = cur
+        self.head = new_node
 
-    def addAtHead(self, val):
-        pass
+    def addAtTail(self, val: int) -> None:
+        new_node = Node(val)
+        if self.head is None:
+            self.head = new_node
+            return
+        
+        cur = self.head
+        while cur.next:
+            cur = cur.next
+        cur.next = new_node
 
-    def addAtTail(self, val):
-        pass
+    def addAtIndex(self, index: int, val: int) -> None:
+        
 
-    def addAtIndex(self, index, val):
-        pass
+    def deleteAtIndex(self, index: int) -> None:
 
-    def deleteAtIndex(self, index):
-        pass
+
+
+# Your MyLinkedList object will be instantiated and called as such:
+# obj = MyLinkedList()
+# param_1 = obj.get(index)
+# obj.addAtHead(val)
+# obj.addAtTail(val)
+# obj.addAtIndex(index,val)
+# obj.deleteAtIndex(index)
 
