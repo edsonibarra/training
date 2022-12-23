f70f804687e434b1c4cd8beaecfe13c50cfbf53c
edson ibarra <edsonibarra97@gmail.com>
2022-12-22 21:09:46 -0600

    Node implementation

--- src/challenges/design_linked_list.py
+++ src/challenges/design_linked_list.py
@@ -1,1 +1,21 @@
+class MyLinkedList:
+    def __init__(self):
+        self.head = None
+
+    def get(self, index):
+        count = 0
+        if index == count:
+            
+
+    def addAtHead(self, val):
+        pass
+
+    def addAtTail(self, val):
+        pass
+
+    def addAtIndex(self, index, val):
+        pass
+
+    def deleteAtIndex(self, index):
+        pass
 
