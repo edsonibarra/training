bc6ff828a3926c136bd3be1e92cac7b2dbbaa1cd
edsonibarra <edsonibarra97@gmail.com>
2023-01-14 21:29:38 -0600

    second solution for student attendance record 1

--- src/challenges/student_attendance_record.py
+++ src/challenges/student_attendance_record.py
@@ -3,9 +3,26 @@ def student_attendance_record(s):
         return False
     return True
 
+def student_attendance_record_ll(s):
+    A_count = 0
+    L_count = 0
+    for c in s:
+        if c == "A":
+            A_count += 1
+            L_count = 0
+            if A_count == 2:
+                return False
+        elif c == "L":
+            L_count += 1
+            if L_count == 3:
+                return False
+        elif c == "P":
+            L_count = 0
+    return True
+
 
 def main():
-    days = 'PPALLL' # False
+    days = 'PPALLL'
     print(student_attendance_record(days))
     
 
