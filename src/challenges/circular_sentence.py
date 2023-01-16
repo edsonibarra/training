536fa76c99a39d6d26393705cc006125e66b278e
edsonibarra <edsonibarra97@gmail.com>
2023-01-16 01:13:02 -0600

    [SOLVED] circular sentence

--- src/challenges/circular_sentence.py
+++ src/challenges/circular_sentence.py
@@ -1,2 +1,14 @@
 def is_circular_sentence(sentence):
-    pass
+    first_letter = sentence[0]
+    idx = 0
+    before_space = ""
+    after_space = ""
+    while idx < len(sentence):
+        if not sentence[idx].isalpha():
+            before_space = sentence[idx - 1]
+            after_space = sentence[idx + 1]
+        if before_space != after_space:
+            return False
+        last_letter = sentence[idx]
+        idx += 1
+    return first_letter == last_letter
